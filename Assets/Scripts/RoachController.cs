80ca08fe5ce81c43856720345bc6701545b3f8c8
KD <kirtidhwajsingh95@gmail.com>
2022-09-05 20:27:54 -0600

    roaches spawn and move around

--- Assets/Scripts/RoachController.cs
+++ Assets/Scripts/RoachController.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class RoachController : MonoBehaviour
 {
+    private float speed = 5.0f;
+    private Vector3 destination;
+    private bool isDestinationSet = false;
+    private bool isRotated = false;
+
+    const int BOUNDS_X = 1920;
+    const int BOUNDS_Y = 1080;
+
+    public void SetSpeed(float _speed)
+    {
+        speed = _speed;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,31 @@ public class RoachController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!isDestinationSet)
+        {
+            //set new destination
+            destination = new Vector3(Random.Range(-BOUNDS_X / 20, BOUNDS_X / 20), Random.Range(-BOUNDS_Y / 20, BOUNDS_Y / 20), 150f);
+            isDestinationSet = true;
+        }
+        else if(!isRotated)
+        {
+            //rotate towards target destination
+            Vector3 targetDirection = destination - transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: targetDirection);
+            transform.rotation = targetRotation;
+            isRotated = true;
+        }
+        else
+        {
+            //move towards target destination
+            float step = speed * Time.deltaTime;
+            Vector2 newPosition = Vector2.MoveTowards(transform.position, destination, step);
+            transform.position = new Vector3(newPosition.x, newPosition.y, 150f);
+            if (Vector3.Distance(transform.position, destination) < 2)
+            {
+                isDestinationSet = false;
+                isRotated = false;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/RoachSpawner.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoachSpawner : MonoBehaviour
+{
+    const int BOUNDS_X = 1920;
+    const int BOUNDS_Y = 1080;
+
+    [SerializeField] private float spawnInterval = 2.0f;
+    [SerializeField] private float baseSpeed = 5.0f;
+    [SerializeField] private RoachController roachPrefab;
+
+    private float currentTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime >= spawnInterval)
+        {
+            float x = 0, y = 0;
+            //spawn roach
+            int choice = Random.Range(0, 3);
+            switch(choice)
+            {
+                case 0:
+                    x = -BOUNDS_X / 20;
+                    y = Random.Range(-BOUNDS_Y / 20, BOUNDS_Y / 20);
+                    break;
+                case 1:
+                    x = BOUNDS_X / 20;
+                    y = Random.Range(-BOUNDS_Y / 20, BOUNDS_Y / 20);
+                    break;
+                case 2:
+                    y = -BOUNDS_Y / 20;
+                    x = Random.Range(-BOUNDS_X / 20, BOUNDS_X / 20);
+                    break;
+                case 3:
+                    y = BOUNDS_Y / 20;
+                    x = Random.Range(-BOUNDS_X / 20, BOUNDS_X / 20);
+                    break;
+            }
+            var obj = GameObject.Instantiate<RoachController>(roachPrefab, new Vector3(x, y, 150f), Quaternion.identity, this.transform);
+            obj.SetSpeed(baseSpeed);
+            currentTime = 0;
+        }
+    }
+}
